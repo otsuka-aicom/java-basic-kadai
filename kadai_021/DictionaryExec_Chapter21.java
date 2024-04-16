41f3945f83e7a493f09fab309dc17a270f11f4fa
otsuka-aicom <167046669+otsuka-aicom@users.noreply.github.com>
2024-04-16 16:07:53 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,12 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+	public static void main(String[] args) {
+		
+		Dictionary_Chapter21 dic = new Dictionary_Chapter21();
+		
+		String words[] = {"apple", "banana", "grape", "orange"};
+		
+		dic.wordCheck(words);
+	}
+}
